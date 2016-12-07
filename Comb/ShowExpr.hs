12a2aa2f59a228144541c98062cd84c4bea1c946
Alexander Kubenskiy <akoubensky@gmail.com>
2016-12-07 23:18:34 +0300

    Precedence in Show instance

--- Comb/ShowExpr.hs
+++ Comb/ShowExpr.hs
@@ -1,6 +1,6 @@
 module ShowExpr where
 
--- Представление лямбда-выражений
+-- Представление выражений
 data Expression =
     Constant Integer |
     Function String |
@@ -8,14 +8,16 @@ data Expression =
     Lambda String Expression |
     Application Expression Expression
 
--- Текстовое представление выражений
+-- Текстовое представление выражений с использованием приоритетов позиций конструктов
 instance Show Expression where
     showsPrec _ (Constant c) = shows c
     showsPrec _ (Function f) = (f ++)
     showsPrec _ (Variable x) = (x ++)
-    showsPrec _ (Lambda x e) =
-        ("(\\"++) . (x ++) . ('.':) . shows e . (')':)
-    showsPrec _ (Application (Application e1 e2) e3) =
-        ('(':) . shows e1 . (' ':) . shows e2 . (' ':) . shows e3 . (')':)
+    showsPrec p (Lambda x e) | p < 2 =
+            ("\\"++) . (x ++) . ('.':) . showsPrec 1 e
+                             | otherwise =
+            ("(\\"++) . (x ++) . ('.':) . showsPrec 1 e . (')':)
+    showsPrec 3 (Application e1 e2) =
+            ('(':) . showsPrec 2 e1 . (' ':) . showsPrec 3 e2 . (')':)
     showsPrec _ (Application e1 e2) =
-        ('(':) . shows e1 . (' ':) . shows e2 . (')':)
+            showsPrec 2 e1 . (' ':) . showsPrec 3 e2
